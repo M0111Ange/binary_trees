32e3f6e27dfeda5e6fadb7f91f1d56089250fe2d
vincetoby <vincetobechukwu@gmail.com>
2024-02-01 15:50:19 -0800

    update repo

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new_n->left = parent->left;
-		parent->left->parent = new_node;
+		parent->left->parent = new_n;
 	}
 	parent->left = new_n;
 	return (new_n);
